d238c9d48b2fdb640e4fff3c49bc2faf8e1d5074
Davit Khudaverdyan <davkhuda@gmail.com>
2022-09-26 08:39:28 -0400

    Implement linear search with swap, binary search, and recursive binary search methods

--- c_cpp/ArrayADT/ArrayADT.c
+++ c_cpp/ArrayADT/ArrayADT.c
@@ -33,13 +33,77 @@ struct Array {
     }
   }
 
+  void Delete(struct Array *arr, int loc) {
+    int i;
+    if (loc >= 0 && loc <= arr->length) {
+      for (i = loc; i < arr->length-1; i++){
+        arr->A[i] = arr->A[i+1];
+      }
+    }
+    arr->A[arr->length] = 0;
+    arr->length--;
+  }
+
+  void swap(int *x, int *y) {
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+  }
+
+  int LinearSearch(struct Array *arr, int searchItem) {
+    int i;
+
+    for(i = 0; i < arr->length; i++) {
+      if (arr->A[i] == searchItem){
+        swap(&arr->A[i], &arr->A[0]); // Swap found item with index 0 item. If the item is searched for frequently it will take a shorter time to search for that item
+        return i;
+      }
+    }
+
+    return -1;
+  }
+
+  // Binary search requires that the list is sorted
+  int BinarySearch(struct Array arr, int searchItem) {
+    int lower, higher, mid;
+    lower = 0;
+    higher = arr.length-1;
+    mid = (lower + higher) / 2;
+
+    while (lower <= higher) {
+      if (arr.A[mid] == searchItem)
+        return mid;
+      else if (searchItem < arr.A[mid])
+        higher = mid - 1;
+      else
+        lower = mid + 1;
+
+      mid = (lower + higher) / 2;
+    }
+    return -1;
+  }
+
+  int RBinarySearch(int arr[], int lower, int higher, int searchItem) {
+    int mid;
+
+    mid = (lower + higher) / 2;
+
+    if (lower <= higher) {
+      if(arr[mid] == searchItem) 
+        return mid;
+      else if (searchItem < arr[mid])
+        return RBinarySearch(arr, lower, mid - 1, searchItem);
+      else
+        return RBinarySearch(arr, mid + 1, higher, searchItem);
+    }
+
+    return -1;
+  }
 int main() {
 
   struct Array arr= {{2,3,4,5,6,7,8}, 10, 7};
 
-  Display(arr);
-  Insert(&arr, 5, 10);
-
-  Display(arr);
+  printf("Binary Search result: %d", RBinarySearch(arr.A, 0, arr.length - 1, 3));
   return 0;
 }
